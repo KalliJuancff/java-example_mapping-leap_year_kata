064c0182f670680184aa75f47d54ad0afdd49ef0
Juan Carlos Fidalgo Fernández <juancfoofn@yahoo.com>
2024-10-27 17:08:44 +0100

    refactor(sut): add several helper methods in LeapYearChecker

--- src/main/java/LeapYearChecker.java
+++ src/main/java/LeapYearChecker.java
@@ -1,17 +1,33 @@
 public class LeapYearChecker {
     public boolean isLeapYear(int year) {
-        if (year % 4 != 0) {
+        if (!isDivisibleBy4(year)) {
             return false;
         }
 
-        if (year % 100 != 0) {
+        if (!isDivisibleBy100(year)) {
             return true;
         }
 
-        if (year % 400 != 0) {
+        if (!isDivisibleBy400(year)) {
             return false;
         }
 
         return true;
+    }
+
+    private static boolean isDivisibleBy4(int year) {
+        return isYearDivisibleBy(year, 4);
+    }
+
+    private static boolean isDivisibleBy100(int year) {
+        return isYearDivisibleBy(year, 100);
+    }
+
+    private static boolean isDivisibleBy400(int year) {
+        return isYearDivisibleBy(year, 400);
+    }
+
+    private static boolean isYearDivisibleBy(int year, int divisor) {
+        return year % divisor == 0;
     }
 }
